a5716f0deb3820e6aeb0164cfa0672dfe9401064
Loik Le Devehat <loik.le-devehat@telecom-paristech.fr>
2014-02-06 01:15:57 +0100

    cleaned repository

--- code/head_tracking/facedetect.h
+++
@@ -1,7 +0,0 @@
-
-int headtrack(int *x1, int *y1, int *x2, int *y2, int lissage, int smooth, int delay, double opt_scale);
-void endThread(void);
-
-
-#define DEPTH_ADJUST 100 // between 1 and 1000, zoom effect
-#define SCALE 40 // scale of head between 10 and 100
